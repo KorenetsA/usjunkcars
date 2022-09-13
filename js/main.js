3695f96e0b28fb90eaeeb42a55027845c7052120
Andrei <korenetsas@gmail.com>
2022-09-13 17:30:42 +0300

    add 3

--- js/main.js
+++ js/main.js
@@ -63,6 +63,28 @@ if (document.querySelector('.accordion-2')) {
     alwaysOpen: false,
   });
 }
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn1: document.querySelector('[data-modal-open-1]'),
+    openModalBtn2: document.querySelector('[data-modal-open-2]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    modalOverlay: document.querySelector('[data-modal-overlay]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn1.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.modalOverlay.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    // document.body.classList.toggle('no-scroll');
+  }
+})();
 $(document).ready(function () {
   $('.slider_review').slick({
     dots: true,
